934d668992d0920f2466eb1adf7e69dac19db5fb
ansellntech <136470702+ansellntech@users.noreply.github.com>
2024-11-21 16:58:53 -0300

    1st Commit
    HTML Content To Start Pages

---
+++ site_project/main.js
@@ -0,0 +1,35 @@
+var lastVisited = "body.html";
+			$(document).ready(()=>{
+				$(document).bind("contextmenu",(e)=> {
+					if(e.button == 2){
+						console.error("Dont Click: Right");
+						e.preventDefault();
+						//location.reload();
+					}
+				});
+				$(".accordion").accordion({collapsible:true, active: false});
+				
+				/*$("#accordion").click((e)=>{
+					this.accordion("option", "active", 1);
+				});*/
+				
+				$("#Frame").load("body.html").show();
+				//$("#accordion2").accordion({collapsible:true});
+				$(".mylink").on("click", (e) =>{
+					//alert("OK");//
+					e.preventDefault();
+					b = $(".mylink");
+					try{
+						$(".mylink").each(el => $(".mylink")[el].style.color = "blue")
+						/*for (i in b){
+							if(i == "length") break;
+							b[i].style.color = "blue";
+						}*/
+					}catch(e){console.error(e.code+": "+e+"\nIndex: "+i)}
+					e.target.style.color = "purple";
+					$("#Frame").slideUp("slow");
+					$("#Frame").load(e.target.href).slideDown("slow");
+					lastVisited = e.target.href;
+					//return
+				});
+			});
