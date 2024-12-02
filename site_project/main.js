e754e83ccc3b5a5d6a71a0df542695c6b9690d76
ansellntech <136470702+ansellntech@users.noreply.github.com>
2024-12-02 19:40:02 -0300

    Update For Load Hide Issue main.js

--- site_project/main.js
+++ site_project/main.js
@@ -27,8 +27,8 @@ var lastVisited = "body.html";
 						}*/
 					}catch(e){console.error(e.code+": "+e+"\nIndex: "+i)}
 					e.target.style.color = "purple";
-					$("#Frame").slideUp("slow").hide().load(e.target.href, (r,s,x)=>{
-						$("#Frame").show().slideDown("slow");
+					$("#Frame").slideUp("slow").load(e.target.href, (r,s,x)=>{
+						$("#Frame").slideDown("slow");
 					});
 					lastVisited = e.target.href;
 					console.log("Last Visited Page is: "+lastVisited);
